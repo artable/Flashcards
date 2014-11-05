eff5f06d89a2a1a78498c7a0a12367f0e8ba2bf8
artable <dealadmin@Deal-MacBook-Air-4.local>
2014-11-04 20:54:26 -0700

    Derp.
    Herp Derp.

--- Flashcards.rb
+++ Flashcards.rb
@@ -29,11 +29,7 @@ card_source = [
 {fall_asleep: "s'endormir"},
 {take_a_walk: "se promener"},
 ]
-test_array = [ # 30 values was waaay to many.
-{a: "1"},
-{b: "2"},
-]
-cards = test_array
+cards = card_source
 # cards = card_source # as to avoid editing card_source
 cards.shuffle! # randomizes card order!
 cards.each do |card|
@@ -46,9 +42,6 @@ cards.each do |card|
     else
     	puts "The correct translation is #{frnch}."
     	cards.push({eng => frnch})
-        puts cards
-        # FRNCH FRNCH! FRNCH! FRY!
     end
   end
-  break if guess == "quit" # This line of code errors. undefined variable guess
 end
