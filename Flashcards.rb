2a6ecaeb885a843492e56fcef6f0b9206b851a23
artable <dealadmin@Deal-MacBook-Air-4.local>
2014-11-05 13:07:42 -0700

    Begin work on total score.
    I need to increment better!

--- Flashcards.rb
+++ Flashcards.rb
@@ -28,13 +28,16 @@ card_source = [
 {go_to_bed: "se coucher"},
 {fall_asleep: "s'endormir"},
 {take_a_walk: "se promener"},
+# Missing a card!!!!!!!!!!
 ]
-# test_array = [
-# {rain: "pleuvoir"},
-# {exit: "sortir"},
-# ]
-# cards = test_array
-cards = card_source
+test_array = [
+{rain: "pleuvoir"},
+{exit: "sortir"},
+]
+cards = test_array
+# cards = card_source
+right = 0 # for later use!
+wrong = 0
 cards.shuffle! # randomizes card order!
 cards.each do |card|
   card.each do |eng, frnch|
@@ -43,12 +46,15 @@ cards.each do |card|
     guess = gets.chomp
     if frnch == guess
       puts "Correct!"
+      right += 1
     else
     	puts "The correct translation is #{frnch}."
       cards.push ({eng => frnch})
+      wrong += 1
     end
   end
 end
+final = right - wrong
 frequencies = Hash.new(0)
 cards.each {|key| frequencies[key] += 1} # Applies the keys to frequency numbers
 frequencies.each do |card, int|
@@ -56,7 +62,11 @@ frequencies.each do |card, int|
     print ""
   else
     int -= 1
-    card.each { |eng, frnch|puts "you missed #{eng} #{int} times." }
+    card.each do |eng, frnch| 
+      puts "you missed #{eng} #{int} times." 
+      total = card_source.count
+      puts "total score: #{final} of #{total}"
+    end
   end
 end
 
